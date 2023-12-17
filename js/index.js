72e10a918557aafa6159d1de706af98c7867f4a1
Glenn Guillard <glenn.guillard.pros@gmail.com>
2023-12-17 22:27:23 +0100

    Continuation Sécu JS et envoie fichier thomas (problème git)

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 window.addEventListener('load', function () {
-    setTimeout(function () {
-      var avion2 = document.getElementById("avion_helice");
-      avion2.style.display = 'block';
-      avion2.style.animationName = "arriver";
-      avion2.style.opacity = '1';
-    }, 2000);
-  });
-  function commence() {
+  setTimeout(function () {
     var avion2 = document.getElementById("avion_helice");
-    var container_avion = document.getElementById("container_avion");
-    avion2.style.animationName = "depart_haut";
+    avion2.style.display = 'block';
+    avion2.style.animationName = "arriver";
+    avion2.style.opacity = '1';
+  }, 2000);
+});
+function commence() {
+  var avion2 = document.getElementById("avion_helice");
+  var container_avion = document.getElementById("container_avion");
+  avion2.style.animationName = "depart_haut";
 
-    avion2.addEventListener('animationend', function () {
-      container_avion.style.opacity = '0';
-    });
-  }
+  avion2.addEventListener('animationend', function () {
+    container_avion.style.opacity = '0';
+  });
+}
+
+/*
+var largeurEcran = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var hauteurEcran = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+// Affichez la taille de l'écran dans la console
+console.log('Largeur de l\'écran : ' + largeurEcran + ' pixels');
+console.log('Hauteur de l\'écran : ' + hauteurEcran + ' pixels');*/
--- script_cree_compte.js
+++ script_cree_compte.js
@@ -1,6 +1,7 @@
 let verif_nom = false;
 let verif_prenom = false;
 let verif_mail = false;
+let verif_num = false;
 
 function verification_nom() {
   const nom = document.getElementById("nom").value;
@@ -32,8 +33,61 @@ function verification_mail() {
   }
   button_disable_abled();
 }
+
+function verification_num() {
+  let num = document.getElementById("num").value;
+
+  const liste_cara = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+  ];
+
+  for (let i = 0; i < num.length; i++) {
+    if (liste_cara.includes(num[i].toUpperCase())) {
+      document.getElementById("num").value =
+        num.substring(0, i) + num.substring(i + 1);
+    }
+  }
+  if (num.length > 10) {
+    document.getElementById("num").value = num.substring(0, 10);
+    verification_num();
+  }
+  if (num.length != 10) {
+    console.log(num.length);
+    verif_num = false;
+  } else {
+    console.log(num.length);
+    console.log("yeah");
+
+    verif_num = true;
+  }
+  button_disable_abled();
+}
 function button_disable_abled() {
-  if (verif_mail && verif_prenom && verif_nom) {
+  if (verif_mail && verif_prenom && verif_nom && verif_num) {
     document.getElementById("push").disabled = false;
   } else {
     document.getElementById("push").disabled = true;
