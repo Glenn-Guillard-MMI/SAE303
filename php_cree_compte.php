16426a1bfdaf63f822b3670b6973cd943629a082
Glenn Guillard <glenn.guillard.pros@gmail.com>
2023-12-25 15:55:08 +0100

    Continuation Back

--- php_cree_compte.php
+++ php_cree_compte.php
@@ -1,15 +1,45 @@
 <form action="_set_up_compt.php" method="post">
+    <label for="genre">Genre</label>
     <input type="radio" id="Homme" name="genre" value="Homme" checked /><label for="Homme">Homme</label>
     <input type="radio" id="Femme" name="genre" value="Femme" /><label for="Femme">Femme</label>
     <input type="radio" id="Autre" name="genre" value="Autre" /><label for="Autre">Autre</label>
+    <br>
+    <label for="Nom">Nom</label>
     <input type="text" name="nom" required id="nom" onkeyup="verification_nom()">
+    <br>
+
+    <label for="prenom">prenom</label>
     <input type="text" name="prenom" required id='prenom' onkeyup="verification_prenom()">
+    <br>
+
+    <label for="email">email</label>
     <input type="mail" name="email" required id="email" onkeyup="verification_mail()">
+    <br>
+
+    <label for="num">num</label>
     <input type="text" name="num" required id="num" onkeyup="verification_num()">
+    <br>
+
+    <label for="birthday">birthday</label>
     <input type="date" name="birthday" required id="birthday" onchange="Vbirthday()">
-    <input type="password" name="password" required id="password" onkeyup="verification_password()">
+    <br>
+
+    <label for="code_addresse">code postale</label>
     <input type="number" name="code_addresse" require id="code_addresse" onkeyup="verif_code()">
+    <br>
+
+    <label for="physique_addresse">physique_addresse</label>
     <input type="text" name="physique_addresse" require id="physique_addresse" onkeyup="verif_physique()">
+    <br>
+
+    <label for="ville">ville</label>
+    <input type="text" name="ville" require id="ville" onkeyup="verification_ville()">
+    <br>
+
+    <label for="password">password</label>
+    <input type="password" name="password" required id="password" onkeyup="verification_password()">
+    <br>
+
     <input type="submit" disabled id="push">
 </form>
 
--- php_modifier_compte.php
+++ php_modifier_compte.php
@@ -16,26 +16,60 @@ if (isset($_SESSION['mail'])){
         foreach ($resultat as $ligne) {
 
     ?>
-<form action="" method="post">
+
+<form action="_modification_compte.php" method="post">
+    <label for="genre">Genre</label>
     <input type="radio" id="Homme" name="genre" value="Homme"
         <?php if($ligne['civilite']=="Homme"){echo "checked";} ?> /><label for="Homme">Homme</label>
     <input type="radio" id="Femme" name="genre" value="Femme"
         <?php if($ligne['civilite']=="Femme"){echo "checked";} ?> /><label for="Femme">Femme</label>
     <input type="radio" id="Autre" name="genre" value="Autre"
         <?php if($ligne['civilite']=="Autre"){echo "checked";} ?> /><label for="Autre">Autre</label>
+    <br>
+    <label for="Nom">Nom</label>
     <input type="text" name="nom" required id="nom" value="<?php echo $ligne['nom'] ?>" onkeyup="verification_nom()">
+    <br>
+
+    <label for="prenom">prenom</label>
     <input type="text" name="prenom" required id='prenom' value="<?php echo $ligne['prenom'] ?>"
         onkeyup="verification_prenom()">
+    <br>
+
+    <label for="email">email</label>
     <input type="mail" name="email" required id="email" value="<?php echo $ligne['mail'] ?>"
         onkeyup="verification_mail()">
+    <br>
+
+    <label for="num">num</label>
     <input type="text" name="num" required id="num" value="<?php echo $ligne['num_tel'] ?>"
         onkeyup="verification_num()">
+    <br>
+
+    <label for="birthday">birthday</label>
     <input type="date" name="birthday" required id="birthday" value="<?php echo $ligne['date_naissance'] ?>"
         onchange="Vbirthday()">
+    <br>
+
+    <label for="code_addresse">code postale</label>
+    <input type="number" name="code_addresse" require id="code_addresse" value="<?php echo $ligne['code_postale'] ?>"
+        onkeyup="verif_code()">
+    <br>
+
+    <label for="physique_addresse">physique_addresse</label>
+    <input type="text" name="physique_addresse" require id="physique_addresse" value="<?php echo $ligne['adresse'] ?>"
+        onkeyup="verif_physique()">
+    <br>
+
+    <label for="ville">ville</label>
+    <input type="text" name="ville" require id="ville" value="<?php echo $ligne['ville'] ?>"
+        onkeyup="verification_ville()">
+    <br>
+
+    <!-- <label for="password">password</label>
     <input type="password" name="password" required id="password" onkeyup="verification_password()">
-    <input type="number" name="code_addresse" require id="code_addresse" onkeyup="verif_code()">
-    <input type="text" name="physique_addresse" require id="physique_addresse" onkeyup="verif_physique()">
-    <input type="submit" disabled id="push">
+    <br> -->
+    <b>Après avoir modifier vos informations, vous serez déconnecter automatiquement</b>
+    <input type="submit" id="pushe">
 </form>
 
 <?php
