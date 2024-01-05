1643cdaef4cde19d94d7e0a0673f491e1be8f539
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-01-05 22:30:25 +0100

    Gestion reservation faite

---
+++ _accepteReservationforfait.php
@@ -0,0 +1,36 @@
+<?php
+
+//initialisation de données
+if(!empty($_POST["id"]) ){
+
+
+//Recupération POO
+require_once "poo_repository.php";
+require_once "poo_models.php";
+
+
+//Création variables
+
+$id =($_POST["id"]);
+
+
+
+
+
+//Lancement de la requete
+try{
+
+    $modele = new Model("r_forfait");
+	$Repository = new Repository($modele->getTable());
+    $sql ="UPDATE ". $modele->getTable() ." SET validation = 1 WHERE num_res = $id";
+    $Repository->requete($sql);
+    
+
+}
+catch(PDOException $e){
+   
+    die($e->getMessage());
+}}
+    else{
+    echo"ERROR";
+    }
--- php_gestionReservation.php
+++ php_gestionReservation.php
@@ -429,6 +429,112 @@
 
 
             ?>
+      <section class="w-75 ml-custom mb-5">
+          <table>
+              <thead class="">
+                  <tr class="text-white">
+                      <th>Id</th>
+                      <th>Nom et Prénom</th>
+                      <th>Type</th>
+                      <th>Plus d'action</th>
+                  </tr>
+              </thead>
+              <tbody>
+
+                  <?php
+
+                            try {
+
+                                $modele2 = new Model("r_forfait");
+                                $exemple2 = new Repository($modele2->getTable());
+                                $sql2 = "Select * from " . $modele2->getTable() . " where validation = 0 ";
+                                $resultat2 = $exemple2->requete($sql2);
+
+                                foreach ($resultat2 as $ligne2) {
+                            ?>
+
+                  <tr>
+                      <th><?= $ligne2["num_res"] ?></th>
+                      <th>
+
+                          <?php
+                                            try {
+                                                $mailadherant = "'" . $ligne2["mail"] . "'";
+                                                $modele3 = new Model("adherant");
+                                                $exemple3 = new Repository($modele3->getTable());
+                                                $sql3 = "Select * from " . $modele3->getTable() . " where mail = $mailadherant";
+                                                $resultat3 = $exemple3->requete($sql3);
+
+                                                foreach ($resultat3 as $ligne3) {
+                                                    echo $ligne3["nom"] . " " . $ligne3["prenom"];
+                                                }
+                                            } catch (PDOException $e) {
+                                                die($e->getMessage());
+                                            }
+                                            ?>
+                      </th>
+                      <th>
+
+
+                          <?php  try {
+                                                $type = $ligne2["id"]; 
+                                                $modele3 = new Model("forfait");
+                                                $exemple3 = new Repository($modele3->getTable());
+                                                $sql3 = "Select * from " . $modele3->getTable() . " where id = $type";
+                                                $resultat3 = $exemple3->requete($sql3);
+
+                                                foreach ($resultat3 as $ligne3) {
+                                                    echo $ligne3["nom"];
+                                                }
+                                            } catch (PDOException $e) {
+                                                die($e->getMessage());
+                                            }
+                                                ?>
+
+                      </th>
+
+
+                      <th>
+
+                          <span class="d-flex flex-row align-items-center justify-content-around">
+                              <p class="p-2 icon-shadow rounded-3 mb-0 icon_confirm"
+                                  onclick="AccepterForfait(<?= $ligne2['num_res'] ?>)">
+                                  <i class="fas fa-check text-success px-1"></i>
+                              </p>
+                              <p class="p-2 icon-shadow rounded-3 mb-0 icon_confirm"
+                                  onclick="RefuserForfait(<?= $ligne2['num_res'] ?>)">
+                                  <i class="fas fa-times text-danger px-1"></i>
+                              </p>
+                          </span>
+                      </th>
+                  </tr>
+
+
+                  <?php
+
+
+                                }
+                                ?>
+
+              </tbody>
+          </table>
+      </section>
+      </div>
+
+
+
+
+      <?php
+                            } catch (PDOException $e) {
+                                die($e->getMessage());
+                            }
+
+
+            ?>
+
+
+
+
       <div id="bapteme-valid">
           <p class="fs-4">Liste de réservation pour les baptêmes de l'air</p>
           <table>
@@ -514,7 +620,7 @@
                                             echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-success w-custom px-3 py-1 rounded-4 statu-shadow'>";
                                             echo "<i class='fas fa-circle me-2 mb-0'></i>";
                                             echo "<p class='m-0'> Acceptée </p>";
-                                        } ?><?php if ($ligne2["validation"] == 0) {
+                                        } ?><?php if ($ligne2["validation"] == 2) {
                                                 echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-danger w-custom px-3 py-1 rounded-4 statu-shadow'>";
                                                 echo "<i class='fas fa-circle me-2 mb-0'></i>";
                                                 echo "<p class='m-0'>Refusée</p>";
@@ -545,6 +651,188 @@
         ?>
 
       </section>
+
+      <section class="w-75 ml-custom mb-5">
+          <table>
+              <thead class="">
+                  <tr class="text-white">
+                      <th>Id</th>
+                      <th>Nom et Prénom</th>
+                      <th>Type</th>
+                      <th>Etat</th>
+                  </tr>
+              </thead>
+              <tbody>
+
+                  <?php
+
+                            try {
+
+                                $modele2 = new Model("r_forfait");
+                                $exemple2 = new Repository($modele2->getTable());
+                                $sql2 = "Select * from " . $modele2->getTable() . " where validation != 0 ";
+                                $resultat2 = $exemple2->requete($sql2);
+
+                                foreach ($resultat2 as $ligne2) {
+                            ?>
+
+                  <tr>
+                      <th><?= $ligne2["num_res"] ?></th>
+                      <th>
+
+                          <?php
+                                            try {
+                                                $mailadherant = "'" . $ligne2["mail"] . "'";
+                                                $modele3 = new Model("adherant");
+                                                $exemple3 = new Repository($modele3->getTable());
+                                                $sql3 = "Select * from " . $modele3->getTable() . " where mail = $mailadherant";
+                                                $resultat3 = $exemple3->requete($sql3);
+
+                                                foreach ($resultat3 as $ligne3) {
+                                                    echo $ligne3["nom"] . " " . $ligne3["prenom"];
+                                                }
+                                            } catch (PDOException $e) {
+                                                die($e->getMessage());
+                                            }
+                                            ?>
+                      </th>
+                      <th>
+
+
+                          <?php  try {
+                                                $type = $ligne2["id"]; 
+                                                $modele3 = new Model("forfait");
+                                                $exemple3 = new Repository($modele3->getTable());
+                                                $sql3 = "Select * from " . $modele3->getTable() . " where id = $type";
+                                                $resultat3 = $exemple3->requete($sql3);
+
+                                                foreach ($resultat3 as $ligne3) {
+                                                    echo $ligne3["nom"];
+                                                }
+                                            } catch (PDOException $e) {
+                                                die($e->getMessage());
+                                            }
+                                                ?>
+
+                      </th>
+
+
+                      <th>
+
+                          <?php if ($ligne2["validation"] == 1) {
+                                            echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-success w-custom px-3 py-1 rounded-4 statu-shadow'>";
+                                            echo "<i class='fas fa-circle me-2 mb-0'></i>";
+                                            echo "<p class='m-0'> Acceptée </p>";
+                                        } ?><?php if ($ligne2["validation"] == 2) {
+                                                echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-danger w-custom px-3 py-1 rounded-4 statu-shadow'>";
+                                                echo "<i class='fas fa-circle me-2 mb-0'></i>";
+                                                echo "<p class='m-0'>Refusée</p>";
+                                            } ?>
+                          </span>
+                          </span>
+                      </th>
+                  </tr>
+
+
+                  <?php
+
+
+                                }
+                                ?>
+                  <?php
+
+try {
+
+    $modele2 = new Model("r_formation");
+    $exemple2 = new Repository($modele2->getTable());
+    $sql2 = "Select * from " . $modele2->getTable() . " where validation != 0 ";
+    $resultat2 = $exemple2->requete($sql2);
+
+    foreach ($resultat2 as $ligne2) {
+?>
+
+                  <tr>
+                      <th><?= $ligne2["num_res"] ?></th>
+                      <th>
+
+                          <?php
+                try {
+                    $mailadherant = "'" . $ligne2["mail"] . "'";
+                    $modele3 = new Model("adherant");
+                    $exemple3 = new Repository($modele3->getTable());
+                    $sql3 = "Select * from " . $modele3->getTable() . " where mail = $mailadherant";
+                    $resultat3 = $exemple3->requete($sql3);
+
+                    foreach ($resultat3 as $ligne3) {
+                        echo $ligne3["nom"] . " " . $ligne3["prenom"];
+                    }
+                } catch (PDOException $e) {
+                    die($e->getMessage());
+                }
+                ?>
+                      </th>
+                      <th>
+
+
+                          <?php  try {
+                    $type = $ligne2["id"]; 
+                    $modele3 = new Model("formation");
+                    $exemple3 = new Repository($modele3->getTable());
+                    $sql3 = "Select * from " . $modele3->getTable() . " where id = $type";
+                    $resultat4 = $exemple3->requete($sql3);
+
+                    foreach ($resultat4 as $ligne4) {
+                        echo $ligne4["nom"];
+                    }
+                } catch (PDOException $e) {
+                    die($e->getMessage());
+                }
+                    ?>
+
+                      </th>
+
+
+                      <th>
+
+                          <?php if ($ligne2["validation"] == 1) {
+                echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-success w-custom px-3 py-1 rounded-4 statu-shadow'>";
+                echo "<i class='fas fa-circle me-2 mb-0'></i>";
+                echo "<p class='m-0'> Acceptée </p>";
+            } ?><?php if ($ligne2["validation"] == 2) {
+                    echo "<span class='d-flex flex-row justify-content-start align-items-center ms-3 mb-0 text-danger w-custom px-3 py-1 rounded-4 statu-shadow'>";
+                    echo "<i class='fas fa-circle me-2 mb-0'></i>";
+                    echo "<p class='m-0'>Refusée</p>";
+                } ?>
+                          </span>
+                          </span>
+                      </th>
+                  </tr>
+
+
+                  <?php
+
+
+    }
+    ?>
+              </tbody>
+          </table>
+      </section>
+      </div>
+
+
+
+
+      <?php
+                            } catch (PDOException $e) {
+                                die($e->getMessage());
+                            }
+                        } catch (PDOException $e) {
+                            die($e->getMessage());
+                        }
+
+
+            ?>
+
       </div>
 
       <script src="node_modules/jquery/dist/jquery.js?time=<?php
